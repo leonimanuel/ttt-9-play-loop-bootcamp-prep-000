416700fc7c9ebef69d18e412992016adb57b375a
Leon Malisov <leonmalisov@gmail.com>
2019-11-29 20:58:37 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -23,7 +23,7 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board, current_player)
+def turn(board, current_player = "X")
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
